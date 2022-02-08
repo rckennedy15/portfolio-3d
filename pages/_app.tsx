d9f63b536d6f06457940c9e5dfa43023c291be00
rckennedy15 <rckennedy15@gmail.com>
2022-02-08 14:16:14 -0500

    add analytics and GSAP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Script id='analytics' type='text/javascript'>{`
+		  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+			m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+			ym(87442287, "init", {
+				clickmap:true,
+				trackLinks:true,
+				accurateTrackBounce:true,
+				webvisor:true
+			});
+			`}</Script>
+			<noscript>
+				<div>
+					<img
+						src='https://mc.yandex.ru/watch/87442287'
+						style={{ position: 'absolute', left: '-9999px' }}
+						alt=''
+					/>
+				</div>
+			</noscript>
+			{/* GSAP libraries from CDN */}
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js' />
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/Flip.min.js' />
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js' />
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollToPlugin.min.js' />
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/MotionPathPlugin.min.js' />
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/TextPlugin.min.js' />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
