81a6a5ef30a8c4f6078da7dc6fba8ecae846583c
rckennedy15 <rckennedy15@gmail.com>
2022-02-08 17:54:08 -0500

    fix analytics and add description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import '../styles/globals.css';
 import Script from 'next/script';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Head>
+				<title>Ryan Kennedy | Web Developer</title>
+				<meta
+					name='description'
+					content='Hire a Professional Freelance Web Developer. Building clean, animated, and interactive experiences with a focus on security and ethics.'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			{/* Yandex analytics */}
 			<Script id='analytics' type='text/javascript'>{`
 		  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
 			m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -17,7 +27,17 @@ function MyApp({ Component, pageProps }: AppProps) {
 				webvisor:true
 			});
 			`}</Script>
-			<noscript>
+
+			{/* GSAP libraries from CDN */}
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js' />
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/Flip.min.js' />
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js' />
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollToPlugin.min.js' />
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/MotionPathPlugin.min.js' />
+			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/TextPlugin.min.js' />
+
+			<noscript id='noscript-analytics'>
+				{/* NoScript yandex analytics */}
 				<div>
 					<img
 						src='https://mc.yandex.ru/watch/87442287'
@@ -26,13 +46,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 					/>
 				</div>
 			</noscript>
-			{/* GSAP libraries from CDN */}
-			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js' />
-			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/Flip.min.js' />
-			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js' />
-			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollToPlugin.min.js' />
-			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/MotionPathPlugin.min.js' />
-			<Script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/TextPlugin.min.js' />
+
 			<Component {...pageProps} />
 		</>
 	);
