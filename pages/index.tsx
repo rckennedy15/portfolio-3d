292a261a8461d76e61df9c4fa4c2af22d879a3e8
rckennedy15 <rckennedy15@gmail.com>
2022-02-10 13:32:06 -0500

    coming soon landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,103 +1,75 @@
 import type { NextPage } from 'next';
-import Image from 'next/image';
-import { Spin as Hamburger } from 'hamburger-react';
-import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import { RoughEase } from 'gsap/dist/EasePack';
-import { TextPlugin } from 'gsap/dist/TextPlugin';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
-	gsap.registerPlugin(TextPlugin, RoughEase);
-	const cursor = useRef<HTMLHeadingElement>(null);
-	const hi = useRef<HTMLHeadingElement>(null);
-	const text = useRef<HTMLHeadingElement>(null);
-	const hero1 = useRef<HTMLHeadingElement>(null);
-	const hero2 = useRef<HTMLHeadingElement>(null);
+	const calculateTimeLeft = () => {
+		let year = new Date().getFullYear();
+		let difference = +new Date(`3/16/${year}`) - +new Date();
 
-	const words = ['developer.', 'freelancer.', 'designer.'];
+		let timeLeft: {
+			days: number;
+			hours: number;
+			minutes: number;
+			seconds: number;
+		} = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+
+		if (difference > 0) {
+			timeLeft = {
+				days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+				hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+				minutes: Math.floor((difference / 1000 / 60) % 60),
+				seconds: Math.floor((difference / 1000) % 60),
+			};
+		}
+		return timeLeft;
+	};
+
+	const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
 	useEffect(() => {
-		// cursor effect
-		gsap.to(cursor.current, {
-			opacity: 0,
-			ease: 'power4.inOut',
-			repeat: -1,
-			duration: 0.9,
-		});
+		const timer = setTimeout(() => {
+			setTimeLeft(calculateTimeLeft());
+		}, 1000);
 
-		const initTl = gsap.timeline();
-		initTl
-			.from(hero1.current, {
-				duration: 1,
-				y: '4.5rem',
-				ease: 'power3.out',
-			})
-			.from(hero2.current, {
-				duration: 1,
-				y: '4.5rem',
-				ease: 'power3.out',
-				// @ts-ignore
-				onComplete: () => mainTl.play(),
-			});
-
-		initTl.on;
-
-		const mainTl = gsap.timeline({ repeat: -1 }).pause();
-		words.forEach((word) => {
-			let tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1.4 });
-			tl.to(text.current, {
-				duration: 1.2,
-				text: word,
-				ease: 'sine.in',
-			});
-			mainTl.add(tl);
-		});
+		return () => clearTimeout(timer);
 	});
+
 	return (
 		<>
-			<section id='home' className='bg-zinc-900 h-screen'>
-				<nav className='w-screen h-20 flex flex-row-reverse p-4'>
-					<Hamburger
-						color='white'
-						rounded
-						label='show menu'
-						direction='right'
-						onToggle={(toggled) => {
-							if (toggled) {
-								// open a menu
-							} else {
-								// close menu
-							}
-						}}
-					/>
-				</nav>
-				<div className='p-8'>
-					<div>
-						<h1 className='text-7xl text-white font-handwritten2 overflow-hidden'>
-							<span ref={hero1} className='inline-block'>
-								Hi,
-							</span>
-						</h1>
-						<h1 className='text-7xl text-white font-handwritten2 overflow-hidden'>
-							<span ref={hero2} className='inline-block'>
-								I&apos;m Ryan
-							</span>
-						</h1>
+			<section className='flex flex-col items-center justify-center justify-items-center h-screen w-screen bg-zinc-300'>
+				<div className='grow w-full flex items-center justify-center'>
+					<h1 className='text-center text-6xl'>COMING SOON</h1>
+				</div>
+				<div className='w-[70vw] h-[50vh] bg-zinc-800 grid grid-cols-2 md:grid-cols-4 grid-rows-2 md:grid-rows-1 gap-4 justify-center content-center p-10'>
+					<div className='rounded-lg bg-slate-500 flex flex-col items-center justify-center overflow-hidden'>
+						<h2 className='text-center text-white text-[6vw]'>
+							{timeLeft.days}
+						</h2>
+						<h3 className='text-center text-white text-2xl'>days</h3>
 					</div>
-					<div className='pt-16'>
-						<h4 className='text-3xl text-white font-mono2 overflow-hidden'>
-							<span ref={hi} className='inline-block'>
-								a&nbsp;
-							</span>
-							<span ref={text} className='inline-block'></span>
-							<span ref={cursor}>_</span>
-						</h4>
+					<div className='rounded-lg bg-slate-500 flex flex-col items-center justify-center overflow-hidden'>
+						<h2 className='text-center text-white text-[6vw]'>
+							{timeLeft.hours}
+						</h2>
+						<h3 className='text-center text-white text-2xl'>hours</h3>
+					</div>
+					<div className='rounded-lg bg-slate-500 flex flex-col items-center justify-center overflow-hidden'>
+						<h2 className='text-center text-white text-[6vw]'>
+							{timeLeft.minutes}
+						</h2>
+						<h3 className='text-center text-white text-2xl'>minutes</h3>
+					</div>
+					<div className='rounded-lg bg-slate-500 flex flex-col items-center justify-center overflow-hidden'>
+						<h2 className='text-center text-white text-[6vw]'>
+							{timeLeft.seconds}
+						</h2>
+						<h3 className='text-center text-white text-2xl'>seconds</h3>
 					</div>
 				</div>
+				<div className='grow w-full flex items-center justify-center'>
+					<p>Ⓒ Copyright 2022 Ryan Kennedy</p>
+				</div>
 			</section>
-			<section id='about' className='bg-zinc-600 h-screen'></section>
-			<section id='skills' className='bg-cyan-700 h-screen '></section>
-			<section id='works' className='bg-zinc-600 h-screen'></section>
-			<section id='contact' className='bg-zinc-900 h-screen'></section>
 		</>
 	);
 };
