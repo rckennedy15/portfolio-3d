9053b69a5ffa70dd1d7645441e6dd12b7bef769e
Ryan <rckennedy15@gmail.com>
2022-02-25 23:55:39 -0500

    add social media meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,39 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
+				{/* <!-- Primary Meta Tags --> */}
+				<meta name='title' content='Ryan Kennedy | Web Developer' />
 				<meta
 					name='description'
 					content='Hire a Professional Freelance Web Developer. Building clean, animated, and interactive experiences with a focus on security and ethics.'
 				/>
+
+				{/* <!-- Open Graph / Facebook --> */}
+				<meta property='og:type' content='website' />
+				<meta property='og:url' content='https://metatags.io/' />
+				<meta property='og:title' content='Ryan Kennedy | Web Developer' />
+				<meta
+					property='og:description'
+					content='Hire a Professional Freelance Web Developer. Building clean, animated, and interactive experiences with a focus on security and ethics.'
+				/>
+				<meta
+					property='og:image'
+					content='https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png'
+				/>
+
+				{/* <!-- Twitter --> */}
+				<meta property='twitter:card' content='summary_large_image' />
+				<meta property='twitter:url' content='https://metatags.io/' />
+				<meta property='twitter:title' content='Ryan Kennedy | Web Developer' />
+				<meta
+					property='twitter:description'
+					content='Hire a Professional Freelance Web Developer. Building clean, animated, and interactive experiences with a focus on security and ethics.'
+				/>
+				<meta
+					property='twitter:image'
+					content='https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png'
+				/>
+
 				<meta name='theme-color' content='rgb(39, 39, 42)' />
 				<link rel='icon' href='/favicon.ico' />
 
